68dc5e77c523a417579dfbd16ee7cbe5781bc2f6
Yangguang Li <liyangguangcn@gmail.com>
2023-03-12 16:56:38 -0700

    Add CORS.

--- extension/src/shared/utils.ts
+++ extension/src/shared/utils.ts
@@ -1,12 +1,19 @@
 import {apiKey} from './stores';
 
-export async function fetchApi<ReponseBody>(url, body): Promise<ReponseBody> {
+export async function fetchApi<ReponseBody>(path: string, body: Object): Promise<ReponseBody> {
   return new Promise((resolve, reject) => {
-    apiKey.subscribe(async (value) => {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}${url}`, {method: 'POST', body: JSON.stringify({...body, apiKey: value})})
-      // TODO: add cost here
-      if (!res.ok) reject(new Error(await res.text()))
-      return resolve(res.json());
+    apiKey.subscribe(async (storedKey) => {
+      try {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}${path}`, {
+          method: 'POST',
+          mode: 'cors',
+          body: JSON.stringify({...body, apiKey: storedKey}),
+        })
+        if (!res.ok) reject(new Error(await res.text()))
+        return resolve(res.json());
+      } catch (e) {
+        reject(e);
+      }
     })
   });
 }
---
+++ src/hooks.server.ts
@@ -0,0 +1,22 @@
+import type { Handle } from '@sveltejs/kit';
+
+const EXTENSION_ORIGIN = 'chrome-extension://dpbkckaamhodgomcfilpdllcjloeomfi';
+
+export const handle: Handle = async ({ resolve, event }) => {
+  const response = await resolve(event);
+
+  if (event.url.pathname.startsWith('/api')) {
+    if(event.request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+          'Access-Control-Allow-Origin': EXTENSION_ORIGIN,
+        }
+      });
+    }
+
+    response.headers.append('Access-Control-Allow-Origin', EXTENSION_ORIGIN);
+  }
+
+  return response;
+};
