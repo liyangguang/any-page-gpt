3f211b2e94c2051c7df0e1f81c9b86301d72bfba
Yangguang Li <liyangguangcn@gmail.com>
2023-03-25 23:28:04 -0700

    Open up CORS.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
 
-// const EXTENSION_ORIGIN = 'chrome-extension://dpbkckaamhodgomcfilpdllcjloeomfi';
-const EXTENSION_ORIGIN = 'https://en.wikipedia.org';
-
 export const handle: Handle = async ({ resolve, event }) => {
   const response = await resolve(event);
 
@@ -11,12 +8,12 @@ export const handle: Handle = async ({ resolve, event }) => {
       return new Response(null, {
         headers: {
           'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
-          'Access-Control-Allow-Origin': EXTENSION_ORIGIN,
+          'Access-Control-Allow-Origin': '*',
         }
       });
     }
 
-    response.headers.append('Access-Control-Allow-Origin', EXTENSION_ORIGIN);
+    response.headers.append('Access-Control-Allow-Origin', '*');
   }
 
   return response;
